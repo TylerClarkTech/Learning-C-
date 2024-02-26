aec76f7cf2a058b915bf0056562703de6bdc63a2
Tyler Clark Tech <156118556+TylerClarkTech@users.noreply.github.com>
2024-02-26 18:13:28 -0500

    Add files via upload
    calculate the sum of two variables.

---
+++ Practice Exercises 1/Practice1_Sum.cpp
@@ -0,0 +1,19 @@
+//	Tyler Clark 02/22/24
+//  Sum of Two Numbers
+//		Write a program that stores the integers variables, and
+//		stores the sum of these two in a variable name total.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int val1 = 62;
+	int val2 = 94;
+	int total;
+
+	total = val1 + val2;
+	cout << "The total of val1 and val2 is " << total; 
+
+	return 0;
+}

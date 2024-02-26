4f44e9becdc9b6a50919caf4436e26017e390595
Tyler Clark Tech <156118556+TylerClarkTech@users.noreply.github.com>
2024-02-26 18:00:14 -0500

    Add files via upload

---
+++ Chapter2 Exercises/Chapt2_Exer3_SalesTax.cpp
@@ -0,0 +1,26 @@
+/* Name: Tyler Jordan Clark 
+   Date: 02 / 26 / 24
+
+    3. Sales Tax
+        Write a program that will compute the total sales tax on a $95 purchase.
+        Assume the state sales tax is 4%, and the country sales tax is 2%.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    //make a var for purchase amount, the state tax and the country tax. Make a var to store solution.
+    float purchase = 95;
+    float salesTaxState = 0.04;
+    float salesTaxCountry = 0.02;
+    float totalTax;
+
+    //add the state and country sales tax and multiply it by the purchase amount to get the totalTax
+    totalTax = purchase * (salesTaxState + salesTaxCountry);
+
+    //display totalTax w/ a fixed decimal place of two places
+    cout << "Sales tax on this purchase will be: $"<< setprecision(2) << fixed << totalTax;
+
+}

d99a2485a7362b692804776979045a277382ca6d
Tyler Clark Tech <156118556+TylerClarkTech@users.noreply.github.com>
2024-02-25 21:47:49 -0500

    Started chapter 2, exercise 1

---
+++ Exercise1.cpp
@@ -0,0 +1,18 @@
+//1. Sum of Two Numbers
+//		Write a program that stores the integers 50 and 100 in variables, and
+//		stores the sum of these two in a variable name total.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int val1 = 50;
+	int val2 = 100;
+	int total;
+
+	total = val1 + val2;
+	cout << "The total of val1 and val2 is " << total; 
+
+	return 0;
+}
